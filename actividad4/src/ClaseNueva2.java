1e600192b68f82e82a8a24a47db1e0355610b496
ACC <usu2@example.com>
2021-04-22 14:00:55 +0200

    Nueva clase2 con cambio

---
+++ actividad4/src/ClaseNueva2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author usuario
+ *
+ */
+public class ClaseNueva2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nueva clase2 con cambio");
+	}
+
+}
